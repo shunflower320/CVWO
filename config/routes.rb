284e79acc6117621112f2668b2276265ecb88cb0
Mandy <shunflower320@gmail.com>
2019-01-02 12:34:42 +0800

    Removed folder todomanager

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/todomanager'
   get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :tasks
+
+  root 'welcome#index'
 end
